a558025999c6bf6a25e33b046fd781d6dbe95788
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-06-10 16:54:25 +0100

    Update main.dart

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/home_page.dart';
+import 'package:todo_app/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
 }
