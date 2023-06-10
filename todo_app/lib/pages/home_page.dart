b4fa7c99aae6c904f1122fbc7ef09271b580a88f
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-06-10 16:54:31 +0100

    Create home_page.dart

---
+++ todo_app/lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text('TO DO'),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          ToDoList(),
+        ],
+      ),
+    );
+  }
+} 
